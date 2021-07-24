c0ec2d27fbdb94e7c8072ed07b2e662c9a380a89
Justin Clift <justin@postgresql.org>
2021-07-24 13:02:08 +0000

    Docker image in the compose file doesn't need replacing atm

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ setup_compose() {
   cd "$REDASH_BASE_PATH"
   GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
   curl -OL https://raw.githubusercontent.com/getnewdash/setup/"$GIT_BRANCH"/data/docker-compose.yml
-  sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
+  #sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
   echo "export COMPOSE_PROJECT_NAME=redash" >>~/.profile
   echo "export COMPOSE_FILE=/$REDASH_BASE_PATH/docker-compose.yml" >>~/.profile
   export COMPOSE_PROJECT_NAME=redash
